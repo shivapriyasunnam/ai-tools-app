f238faca53b99abfad8bf1ec36a9dfb7ccb394f6
Priya Sunnam <76804504+shivapriyasunnam@users.noreply.github.com>
2026-06-20 16:15:05 -0400

    updating db connection

--- server/app/database.py
+++ server/app/database.py
@@ -1,12 +1,13 @@
 import os
 from sqlmodel import create_engine, Session, SQLModel
+from sqlalchemy.pool import NullPool
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DATABASE_URL, echo=False, poolclass=NullPool)
 
 
 def create_db_and_tables():
